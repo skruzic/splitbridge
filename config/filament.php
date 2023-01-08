de5a31f85b9c48f90dc495195ecf5cbb1977d93c
Stanko Kruzic <skruzic@gmail.com>
2023-01-08 18:52:21 +0100

    Dodan paket za profil na Filament

--- config/filament.php
+++ config/filament.php
@@ -12,6 +12,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use RyanChandler\FilamentProfile\Pages\Profile;
 
 return [
 
@@ -106,6 +107,7 @@ return [
         'path' => app_path('Filament/Pages'),
         'register' => [
             Pages\Dashboard::class,
+            Profile::class
         ],
     ],
 
