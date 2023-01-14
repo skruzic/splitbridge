d713c3752346d29e28b27623f85d061d47e82f89
Stanko Kruzic <skruzic@gmail.com>
2023-01-14 20:08:14 +0100

    Dodano sucelje za dodavanje administratora

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+    protected static ?string $modelLabel = 'administrator';
+    protected static ?string $pluralModelLabel = 'administratori';
+
+    protected static ?string $navigationIcon = 'heroicon-o-key';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required()->label('Ime i prezime'),
+                TextInput::make('email')->required()->email()->label('E-mail'),
+                TextInput::make('password')
+                         ->required(fn(string $context): bool => $context === 'create')
+                         ->password()
+                         ->label('Šifra')
+                         ->dehydrateStateUsing(fn($state) => Hash::make($state))
+                         ->dehydrated(fn($state) => filled($state)),
+            ])->columns(1);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('email')->label('E-mail'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index'  => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit'   => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
