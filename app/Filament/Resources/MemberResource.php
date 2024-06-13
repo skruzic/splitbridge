14418736197a58865962bc7d0f4cb1f41ca8566c
Stanko Kruzic <skruzic@gmail.com>
2024-06-13 11:57:22 +0200

    Infolista po igracu

--- app/Filament/Resources/MemberResource.php
+++ app/Filament/Resources/MemberResource.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\MemberResource\Pages;
 use App\Models\Member;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\RepeatableEntry;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
@@ -69,20 +70,32 @@ class MemberResource extends Resource
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListMembers::route('/'),
+            'index'  => Pages\ListMembers::route('/'),
             'create' => Pages\CreateMember::route('/create'),
-            'edit' => Pages\EditMember::route('/{record}/edit'),
-            'view' => Pages\ViewMember::route('/{record}'),
+            'edit'   => Pages\EditMember::route('/{record}/edit'),
+            'view'   => Pages\ViewMember::route('/{record}'),
         ];
     }
 
     public static function infolist(Infolist $infolist): Infolist
     {
         return $infolist->schema([
-            TextEntry::make('surname'),
-            TextEntry::make('name'),
-            TextEntry::make('email'),
+            TextEntry::make('surname')->label('Prezime'),
+            TextEntry::make('name')->label('Ime'),
+            TextEntry::make('email')->label('E-mail'),
             TextEntry::make('crobridge')->label('HBS broj'),
+
+            RepeatableEntry::make('ranks')->schema([
+                TextEntry::make('tournament.date')->label('Datum')->date('d.m.Y.'),
+                TextEntry::make('tournament.type')->label('Tip turnira')->badge(),
+                TextEntry::make('rank')->label('Mjesto')->suffix('.'),
+                TextEntry::make('points')->numeric(2)->label('Bodovi'),
+            ])->columns(4),
         ]);
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
     }
 }
