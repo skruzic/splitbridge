edf96881ed12c1a57274a9b7b246b4089f1bf3a8
Stanko Kruzic <skruzic@gmail.com>
2024-06-13 11:22:17 +0200

    Mogucnost direktnog postavljanja tekuce sezone iz admin panela

--- app/Models/Season.php
+++ app/Models/Season.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -12,7 +13,7 @@ class Season extends Model
 
     protected $fillable = ['title'];
 
-    protected function current(): Attribute
+    /*protected function current(): Attribute
     {
         return Attribute::make(get: fn ($value) => $value, set: function ($value) {
             if ($value) {
@@ -21,7 +22,7 @@ class Season extends Model
 
             return $value;
         });
-    }
+    }*/
 
     public static function getCurrent()
     {
