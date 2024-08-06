f1eddf9b3134a06500aed27241bf310ae3db5870
Stanko <skruzic@gmail.com>
2024-08-06 10:31:21 +0200

    Popravljen vite config za build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
@@ -14,7 +14,6 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': '/resources/js',
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        }
-    }
+        },
+    },
 });
